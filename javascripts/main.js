d1fef1ef85e90865cf93dd84606665afbed6193b
Samuel G. Munoz <samuel1698500@gmail.com>
2021-08-19 17:29:21 -0400

    add certificate image carousel

--- javascripts/main.js
+++ javascripts/main.js
@@ -6,13 +6,73 @@ overlay.addEventListener('click', function(){
   this.classList.remove('active');
   overlay.querySelector('.wrapper').classList.remove("active");
   for (let i=0; i < img.length; i++){
+    img[i].tabIndex = -1;
     img[i].classList.remove('active', 'before', 'after');
   }
+  button[0].classList.remove('disabled');
+  button[1].classList.remove('disabled');
 });
 document.querySelector(".cert ul").addEventListener("click", function(e){
   activateOverlay(e);
 });
-
+// Switch the certificate shown to the previous one
+button[0].addEventListener('click', function(event){
+  // Stop overlay click action
+  button[1].classList.remove('disabled');
+  event.stopPropagation();
+  let index = detectActive();
+  if (index !== 0) {
+    resetClasses(index);
+    assignClasses(index - 1);
+  }
+});
+// Switch the certificate shown to the next one
+button[1].addEventListener('click', function(event){
+  // Stop overlay click action
+  button[0].classList.remove('disabled');
+  event.stopPropagation();
+  let index = detectActive();
+  if (index !== (img.length - 1)) {
+    resetClasses(index);
+    assignClasses(index + 1);
+  }
+});
+// Detect which image is the current active one
+function detectActive(){
+  for (let i=0; i < img.length; i++){
+    if (img[i].classList.contains('active') && img[i].classList.length == 2){
+      var index = i;
+      break;
+    }
+  }
+  return index;
+}
+// Completely reset all the active classes for the images
+function resetClasses(i){
+  if (img[i - 1]){
+    img[i - 1].classList.remove('active', 'before');
+  }
+  if (img[i]){
+    img[i].tabIndex = -1;
+    img[i].classList.remove('active');
+  }
+  if (img[i + 1]){
+    img[i + 1].classList.remove('active','after');
+  }
+}
+// Assigns active classes based on the image index
+function assignClasses(i){
+  img[i].tabIndex = 0;
+  img[i].classList.add('active');
+  if (img[i - 1])
+    img[i - 1].classList.add('active', 'before');
+  else
+    button[0].classList.add('disabled');
+  if (img[i + 1])
+    img[i + 1].classList.add('active', 'after');
+  else 
+    button[1].classList.add('disabled');
+};
 function activateOverlay(e){  
   // Activate overlay on click and enter
   overlay.classList.add("active");
@@ -20,35 +80,15 @@ function activateOverlay(e){
   // Add active state to corresponding certificate
   for (let i=0; i < img.length; i++){
     if (img[i].classList[0] == e.target.classList[0]){
-      img[i].classList.add('active');
-      img[i].tabIndex = 0;
-      if (img[i - 1]){
-        img[i - 1].classList.add('active', 'before');
-      }
-      if (img[i + 1]){
-        img[i + 1].classList.add('active', 'after');
-      }
+      assignClasses(i);
       // Add event listener to the image to stop propagation to the overlay
       img[i].addEventListener('click', function(event){
         event.stopPropagation();
       });
     }
   }
-  // Prev
-  button[0].addEventListener('click', function(event){
-    // Stop overlay click action
-    event.stopPropagation();
-    // Switch the certificate shown
-  });
-  // After
-  button[1].addEventListener('click', function(event){
-    // Stop overlay click action
-    event.stopPropagation();
-    // Switch the certificate shown
-    
-  });
 };
- 
+
 const item = document.querySelectorAll('.flex_wrapper .item');
 document.addEventListener('mousemove', function checkHover() {
   for (let i=0; i < item.length; i++){
@@ -72,7 +112,6 @@ document.addEventListener('keydown', function(e){
     }
   }
 });
-
 document.querySelector('.pinkwug .hidden').addEventListener('click', function(e){
   if (!e.target.matches('span') || !e.target.matches('p') || !e.target.matches('a')) {
     window.open('https://pinkwug.live');
