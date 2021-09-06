9742f15b0e502e530aa4a9b9c27db04a52b2950f
Samuel G. Munoz <samuel1698500@gmail.com>
2021-09-06 18:24:07 -0400

    Add form animation of tracking-in-expand and button span animation of wobble

--- javascripts/about.js
+++ javascripts/about.js
@@ -1,1 +1,20 @@
 import "./_dropdown.js";
+
+window.onload = function () {
+  formAnimation();
+}
+window.onscroll = function () {
+  formAnimation();
+};
+function formAnimation() {
+  var form = document.querySelector("main.contact form");
+  if (checkVisible(form)) form.classList.add("animation");
+}
+function checkVisible(elm) {
+  var rect = elm.getBoundingClientRect();
+  var viewHeight = Math.max(
+    document.documentElement.clientHeight,
+    window.innerHeight
+  );
+  return !(rect.bottom < 0 || rect.top - viewHeight >= 0);
+}
