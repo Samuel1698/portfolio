ed9ae247d444503a9733b5c0cee30d15595b4e36
Samuel G. Munoz <samuel1698500@gmail.com>
2021-09-15 17:21:43 -0400

    Add javascript to change submit button's content depending on language selection box. Change box shadow of columns

--- javascripts/_footer-drop.js
+++ javascripts/_footer-drop.js
@@ -1,3 +1,4 @@
+var submit = document.querySelector("footer button[type='submit']");
 var button = document.querySelector("footer button.select-button");
 var ul = document.querySelector("footer ul.select-items");
 var li = ul.querySelectorAll("li.select-item");
@@ -20,9 +21,11 @@ button.addEventListener("keydown", function (e) {
     for (let i = 0; i < li.length; i++) {
       if (e.key == "ArrowUp" && li[i - 1]) {
         this.querySelector("span").innerHTML = li[i - 1].innerHTML;
+        changeLang(li[i - 1]);
       }
       if (e.key == "ArrowDown" && li[i + 1]) {
         this.querySelector("span").innerHTML = li[i + 1].innerHTML;
+        changeLang(li[i + 1]);
       }
       let selected = this.parentElement.querySelector(".same-as-selected");
       selected.classList.remove("same-as-selected");
@@ -46,7 +49,10 @@ button.addEventListener("keydown", function (e) {
 for (let i = 0; i < li.length; i++) {
   li[i].addEventListener("click", function (e) {
     e.stopPropagation();
+    // Make the contents the same
     button.querySelector("span").innerHTML = this.innerHTML;
+    // Change the classes of the submit button spans
+    // submit.querySelector(".button_wrap span")
     let selected =
       this.parentElement.getElementsByClassName("same-as-selected");
     for (k = 0; k < selected.length; k++) {
@@ -56,6 +62,18 @@ for (let i = 0; i < li.length; i++) {
     this.classList.add("same-as-selected");
     this.parentElement.classList.toggle("select-hide");
     button.classList.remove("select-arrow-active");
+    // Assign 'shown' class depending on which list item is selected
+    changeLang(this);
     // Redirect to subdomain on button click depending on the clicked li
   });
+}
+function changeLang(element) {
+  if (element.querySelector("span").innerText == "ES") {
+    submit.querySelector(".ES").classList.add("shown");
+    submit.querySelector(".EN").classList.remove("shown");
+  }
+  else if (element.querySelector("span").innerText == "EN"){
+    submit.querySelector(".EN").classList.add("shown");
+    submit.querySelector(".ES").classList.remove("shown");
+  }
 }
