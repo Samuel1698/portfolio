eda592d3ea4f780844a25f267dc1cae5edde5439
Samuel G. Munoz <samuel1698500@gmail.com>
2023-01-05 10:53:01 -0500

    prevent scroll when header menu is open

--- javascripts/_header.js
+++ javascripts/_header.js
@@ -1,10 +1,11 @@
 const toggleButton = document.querySelector("header .menu-toggle");
 const menu = document.querySelector("header .menu");
 const overlay = document.querySelector("header .overlay");
-
+const body = document.querySelector("body");
 function toggleVisibility(){
   menu.classList.toggle("visible");
   overlay.classList.toggle("visible");
+  body.classList.toggle("no-scroll");
 }
 toggleButton.addEventListener("click", toggleVisibility);
 overlay.addEventListener("click", toggleVisibility);
