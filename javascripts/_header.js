a40fcd10dc7368187aa56b4b22aec0a807873b94
Samuel G. Munoz <samuel1698500@gmail.com>
2023-01-06 00:41:19 -0500

    Header menu restyle. Missing close button, category collapse, syncing and color choice

--- javascripts/_header.js
+++ javascripts/_header.js
@@ -2,6 +2,7 @@ const toggleButton = document.querySelector("header .menu-toggle");
 const menu = document.querySelector("header .menu");
 const overlay = document.querySelector("header .overlay");
 const body = document.querySelector("body");
+
 function toggleVisibility(){
   menu.classList.toggle("visible");
   overlay.classList.toggle("visible");
