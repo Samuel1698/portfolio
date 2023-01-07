8d18f183ec46094b259d01e5408e6120ba3aff81
Samuel G. Munoz <samuel1698500@gmail.com>
2023-01-06 21:35:03 -0500

    Restyle contact page

--- javascripts/_dropdown.js
+++ javascripts/_dropdown.js
@@ -1,11 +1,11 @@
-var wrapper, i, j, label_id, realSelect, button, content, option, span;
+var wrapper, i, j, label_id, realSelect, button, content, option, span, svg, path1, path2;
 // Look for any elements with the class "custom-select":
 // These elements will be used as containers for the custom dropdown menu.
 wrapper = document.getElementsByClassName("inline");
 
 for (i = 0; i < wrapper.length; i++) {
   realSelect = wrapper[i].getElementsByTagName("select")[0];
-  // For each element, create a new DIV that will act as the selected item:
+  // For each element, create a new button that will act as the selected item:
   button = document.createElement("button");
   button.setAttribute("class", "select-button");
   button.tabIndex = 0;
@@ -14,15 +14,41 @@ for (i = 0; i < wrapper.length; i++) {
   label_id = wrapper[i].querySelector("label").id
   button.setAttribute("aria-labelledby", label_id);
   button.setAttribute("aria-expanded", "false");
-  span = document.createElement("SPAN");
+  // Create empty span container and populate it with the selectedItem's content
+  span = document.createElement("span");
   span.innerHTML = realSelect.options[realSelect.selectedIndex].innerHTML;
+  // Create svg of caret down
+  svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+  path1 = document.createElementNS("http://www.w3.org/2000/svg", "path");
+  path2 = document.createElementNS("http://www.w3.org/2000/svg", "path");
+  // svg attributes
+  svg.setAttribute("width", "24");
+  svg.setAttribute("height", "24");
+  svg.setAttribute("viewbox", "0 0 24 24");
+  svg.setAttribute("stroke-width", "1.5");
+  svg.setAttribute("stroke", "#23434F");
+  svg.setAttribute("fill", "none");
+  svg.setAttribute("stroke-linecap", "round");
+  svg.setAttribute("stroke-linejoin", "round");
+  // Path attributes
+  path1.setAttribute("stroke", "none");
+  path1.setAttribute("d", "M0 0h24v24H0z");
+  path1.setAttribute("fill", "none");
+  // Path attributes
+  path2.setAttribute("d", "M18 15l-6 -6l-6 6h12");
+  path2.setAttribute("transform", "rotate(180 12 12)");
+  // add paths to svg
+  svg.appendChild(path1);
+  svg.appendChild(path2);
+
   wrapper[i].appendChild(button);
   button.appendChild(span);
-  // For each element, create a new DIV that will contain the option list:
+  button.appendChild(svg);
+  // For each element, create a new UL that will contain the option list:
   content = document.createElement("ul");
   content.setAttribute("class", "select-items select-hide");
   for (j = 0; j < realSelect.length; j++) {
-    // For each option in the original select element, create a new DIV that will act as an option item:
+    // For each option in the original select element, create a new LI that will act as an option item:
     option = document.createElement("li");
     option.innerHTML = realSelect.options[j].innerHTML;
     option.setAttribute("class", "select-item");
