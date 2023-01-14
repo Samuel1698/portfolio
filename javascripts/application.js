90681f24e3fa717424b6951311a387516fb580ca
Samuel G. Munoz <samuel1698500@gmail.com>
2023-01-14 08:35:22 -0500

    Disable subdomain redirection on live site

--- javascripts/application.js
+++ javascripts/application.js
@@ -1,10 +1,10 @@
 import "./_header";
 import "./_footerDropdown.js";
 
-window.onload = function(){
-  var host = window.location.host
-  var subdomain = host.split('.')[0]
-  if (/^es\b/.test(navigator.language) && subdomain != "en"){
-    window.location.href = "https://es." + window.location.hostname;
-  }
-};
+// window.onload = function(){
+//   var host = window.location.host
+//   var subdomain = host.split('.')[0]
+//   if (/^es\b/.test(navigator.language) && subdomain != "en"){
+//     window.location.href = "https://es." + window.location.hostname;
+//   }
+// };
