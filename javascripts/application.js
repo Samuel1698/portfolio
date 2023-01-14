4232af643c6e2c25bdae781877017a62ae0670a3
Samuel G. Munoz <samuel1698500@gmail.com>
2023-01-14 00:12:51 -0500

    Fix constant redirecing

--- javascripts/application.js
+++ javascripts/application.js
@@ -1,8 +1,10 @@
 import "./_header";
 import "./_footerDropdown.js";
 
-var host = window.location.host
-var subdomain = host.split('.')[0]
-if (/^es\b/.test(navigator.language) && subdomain != "en"){
-  window.location.href = "https://es.samuelgmunoz.com";
+windows.onload = function(){
+  var host = window.location.host
+  var subdomain = host.split('.')[0]
+  if (/^es\b/.test(navigator.language) && subdomain != "en"){
+    window.location.href = "https://es.samuelgmunoz.com";
+  }
 }
