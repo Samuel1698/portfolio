67a785fe3035ae6004a2bede715471e8afa9df34
Samuel G. Munoz <samuel1698500@gmail.com>
2023-02-04 00:17:42 -0500

    Change mockups in projects page

--- javascripts/projects.js
+++ javascripts/projects.js
@@ -1,18 +1,9 @@
-import "./_customHeights";
-// Prepend necessary since we're referencing the same event listener function
-// @prepros-prepend "./_worklist";
+// import "./_customHeights";
 
-document
-  .querySelector(".pinkwug .hidden")
-  .removeEventListener("click", pinkwugListener);
-document
-  .querySelector(".miguel .hidden")
-  .removeEventListener("click", miguelListener);
-
-window.addEventListener("load", () => {
-  var expand = document.querySelectorAll(".bar .expand");
-  for (let i = 0; i < expand.length; i++) {
-    expand[i].parentNode.parentNode.classList.toggle("toggle");
-    expand[i].classList.toggle("on");
-  }
-});
+// window.addEventListener("load", () => {
+//   var expand = document.querySelectorAll(".bar .expand");
+//   for (let i = 0; i < expand.length; i++) {
+//     expand[i].parentNode.parentNode.classList.toggle("toggle");
+//     expand[i].classList.toggle("on");
+//   }
+// });
